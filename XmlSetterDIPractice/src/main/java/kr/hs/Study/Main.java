5e5f1842d16ce501f58cf058d900c683f7baaf35
Hyunji Kim <s2208@e-mirim.hs.kr>
2024-04-30 09:52:39 +0900

    xml setter 주입 방법 추가

---
+++ XmlSetterDIPractice/src/main/java/kr/hs/Study/Main.java
@@ -0,0 +1,18 @@
+package kr.hs.Study;
+
+import kr.hs.Study.Beans.TestBean1;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml");
+
+        TestBean1 obj1 = ctx.getBean("t1", TestBean1.class);
+        System.out.println(obj1.getData1());
+        System.out.println(obj1.getData2());
+
+        ctx.close();
+    }
+}
